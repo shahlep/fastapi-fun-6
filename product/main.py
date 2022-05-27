970bfd80e07ddcc8d76140e654af8046a1e0f709
Shah rahman <toshah.rahman@gmail.com>
2022-05-27 16:44:05 +0300

    fastapi fun 2

--- product/main.py
+++ product/main.py
@@ -17,9 +17,9 @@ def get_db():
 
 @app.post("/product")
 def add(request: schemas.Product, db: Session = Depends(get_db)):
-    new_product = models.Product(name=request.name,
-                                 description=request.description,
-                                 price=request.price)
+    new_product = models.Product(
+        name=request.name, description=request.description, price=request.price
+    )
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
