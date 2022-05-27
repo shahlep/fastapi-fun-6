a30bae8e5bd974747486af7cdf89bbbd44ee525e
Shah rahman <toshah.rahman@gmail.com>
2022-05-27 20:47:18 +0300

    fastapi fun 17

--- product/models.py
+++ product/models.py
@@ -8,3 +8,11 @@ class Product(Base):
     name = Column(String)
     description = Column(String)
     price = Column(Integer)
+
+
+class Seller(Base):
+    __tablename__ = "sellers"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
