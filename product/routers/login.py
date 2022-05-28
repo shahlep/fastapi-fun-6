33505dfb8cd3237bd9d80f7c1802661857826b1d
Shah rahman <toshah.rahman@gmail.com>
2022-05-28 14:20:17 +0300

    fastapi fun 40

--- product/routers/login.py
+++ product/routers/login.py
@@ -8,7 +8,14 @@ router = APIRouter(
     tags=['login'],
 )
 
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 
 @router.post('/login')
 def login(request: schemas.Login, db: Session = Depends(get_db)):
+    user = db.query(models.Seller).filter(
+        models.Seller.username == request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Username not found!')
     return request
